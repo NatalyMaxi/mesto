933c2e4524a787e320f909655ef6a165ac27e697
Natalya Maximova <natka-31@mail.ru>
2022-03-29 11:06:09 +0500

    написать js

--- scripts/index.js
+++ scripts/index.js
@@ -6,3 +6,20 @@ function toggleModalWindow() {
 }
 profileEditingButton.addEventListener('click', toggleModalWindow);
 modalCloseBtn.addEventListener('click', toggleModalWindow);
+
+
+let formElement = document.querySelector('.form');
+let nameInput = document.querySelector('[name="full-name"]');
+let jobinput = document.querySelector('[name="about-me"]');
+let profileTitle = document.querySelector('.profile__title');
+let profileSubtitle = document.querySelector('.profile__subtitle');
+nameInput.value = profileTitle.textContent;
+jobinput.value = profileSubtitle.textContent;
+
+function formSubmitHandler(evt) {
+  evt.preventDefault();
+  profileTitle.textContent = nameInput.value;
+  profileSubtitle.textContent = jobinput.value;
+  toggleModalWindow()
+}
+formElement.addEventListener('submit', formSubmitHandler); 
