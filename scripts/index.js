26e34e15e23fa7944f7b499adf37cb9431df709c
Natalya Maximova <natka-31@mail.ru>
2022-03-31 22:07:08 +0500

    исправить ошибки

--- scripts/index.js
+++ scripts/index.js
@@ -1,20 +1,17 @@
 const profileEditingButton = document.querySelector('.profile__btn');
 const modalWindow = document.querySelector('.popup');
 const modalCloseBtn = modalWindow.querySelector('.popup__close');
-function toggleModalWindow() {
-  modalWindow.classList.toggle('popup_is-active');
-}
-profileEditingButton.addEventListener('click', toggleModalWindow);
-modalCloseBtn.addEventListener('click', toggleModalWindow);
-
-
 let formElement = document.querySelector('.form');
 let nameInput = document.querySelector('[name="full-name"]');
 let jobinput = document.querySelector('[name="about-me"]');
 let profileTitle = document.querySelector('.profile__title');
 let profileSubtitle = document.querySelector('.profile__subtitle');
-nameInput.value = profileTitle.textContent;
-jobinput.value = profileSubtitle.textContent;
+
+function toggleModalWindow() {
+  modalWindow.classList.toggle('popup_is-active');
+  nameInput.value = profileTitle.textContent;
+  jobinput.value = profileSubtitle.textContent;
+}
 
 function formSubmitHandler(evt) {
   evt.preventDefault();
@@ -22,4 +19,8 @@ function formSubmitHandler(evt) {
   profileSubtitle.textContent = jobinput.value;
   toggleModalWindow()
 }
+
+profileEditingButton.addEventListener('click', toggleModalWindow);
+modalCloseBtn.addEventListener('click', toggleModalWindow);
+
 formElement.addEventListener('submit', formSubmitHandler); 
