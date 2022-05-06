b8a83b61a1267967a3576b09ec37f5480b300830
Natalya Maximova <natka-31@mail.ru>
2022-05-06 19:02:41 +0500

    фикс

--- scripts/validate.js
+++ scripts/validate.js
@@ -1,22 +1,22 @@
-const showInputError = (formElement, inputElement, errorMessage) => {
+const showInputError = (formElement, inputElement, errorMessage, config) => {
    const errorElement = formElement.querySelector(`.${inputElement.id}-error`);
-   inputElement.classList.add('form__item_type_error');
+   inputElement.classList.add(config.inputErrorClass);
    errorElement.textContent = errorMessage;
-   errorElement.classList.add('form__error_visible');
+   errorElement.classList.add(config.errorClass);
 };
 
-const hideInputError = (formElement, inputElement,) => {
+const hideInputError = (formElement, inputElement, config) => {
    const errorElement = formElement.querySelector(`.${inputElement.id}-error`);
-   inputElement.classList.remove('form__item_type_error');
-   errorElement.classList.remove('form__error_visible');
+   inputElement.classList.remove(config.inputErrorClass);
+   errorElement.classList.remove(config.errorClass);
    errorElement.textContent = '';
 };
 
-const checkInputValidity = (formElement, inputElement) => {
+const checkInputValidity = (formElement, inputElement, config) => {
    if (!inputElement.validity.valid) {
-      showInputError(formElement, inputElement, inputElement.validationMessage);
+      showInputError(formElement, inputElement, inputElement.validationMessage, config);
    } else {
-      hideInputError(formElement, inputElement);
+      hideInputError(formElement, inputElement, config);
    }
 };
 
@@ -28,7 +28,7 @@ const setEventListeners = (formElement, config) => {
 
    inputList.forEach((inputElement) => {
       inputElement.addEventListener('input', function () {
-         checkInputValidity(formElement, inputElement);
+         checkInputValidity(formElement, inputElement, config);
          toggleButton(inputList, buttonElement, config);
       });
    });
