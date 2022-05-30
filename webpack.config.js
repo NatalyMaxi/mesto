780072e8b35ba2fa281c8e86c7a8215ee31aabd2
Natalya Maximova <natka-31@mail.ru>
2022-05-30 22:41:36 +0500

    заново установленные пакеты

--- webpack.config.js
+++ webpack.config.js
@@ -2,45 +2,37 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
-
 module.exports = {
-   entry: {
-      main: './src/pages/index.js'
-   },
+   entry: { main: './src/pages/index.js' },
    output: {
       path: path.resolve(__dirname, 'dist'),
       filename: 'main.js',
-      publicPath: '',
+      publicPath: ''
    },
    mode: 'development',
-   devtool: 'source-map',
    devServer: {
       static: path.resolve(__dirname, './dist'),
-      open: true,
-      compress: true,
-      port: 8080
+      compress: true, 
+      port: 8080, 
+      open: true 
    },
    module: {
-      rules: [{
-         test: /\.js$/,
-         use: 'babel-loader',
-         exclude: '/node_modules/'
-      },
-      {
-         test: /\.(png|svg|jpg|gif|woff(2)?|eot|ttf|otf)$/,
-         type: 'asset/resource',
-      },
-      {
-         test: /\.css$/,
-         use: [MiniCssExtractPlugin.loader, {
-            loader: 'css-loader',
-            options: {
-               importLoaders: 1
-            }
+      rules: [
+         {
+            test: /\.js$/,
+            use: 'babel-loader',
+            exclude: '/node_modules/'
          },
-            'postcss-loader'
-         ]
-      },
+         {
+            test: /\.(png|svg|jpg|gif|woff(2)?|eot|ttf|otf)$/,
+            type: 'asset/resource'
+         },
+         {
+            test: /\.css$/,
+            use: [MiniCssExtractPlugin.loader, {
+               loader: 'css-loader'
+            }]
+         }
       ]
    },
    plugins: [
@@ -48,7 +40,6 @@ module.exports = {
          template: './src/index.html'
       }),
       new CleanWebpackPlugin(),
-      new MiniCssExtractPlugin(),
-
+      new MiniCssExtractPlugin()
    ]
 }
